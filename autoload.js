3edf3713ef8ae2d8356246d074a758362144ab9b
minyue-hcm <870827153@qq.com>
2021-04-02 15:22:01 +0800

    new live2d widget

--- autoload.js
+++ autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
 const live2d_path =
-  "https://cdn.jsdelivr.net/gh/minyue-hcm/live2d-widget@1.0.1/";
+  "https://cdn.jsdelivr.net/gh/minyue-hcm/live2d-widget@1.0.3/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
